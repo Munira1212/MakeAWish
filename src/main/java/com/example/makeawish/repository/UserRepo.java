ab93ebbeee03accf4642e0bf6b7770b6103d81e0
Ugbaad <ugbaad0012@gmail.com>
2023-04-03 15:03:24 +0200

    hej

--- src/main/java/com/example/makeawish/repository/UserRepo.java
+++ src/main/java/com/example/makeawish/repository/UserRepo.java
@@ -5,6 +5,6 @@ public class UserRepo {
     public void hej () {
         System.out.println("hej");
         //ikhra can u see what i did
-        //ugbaads kommentar
+        //ugbaads kommentarr
     }
 }
