2b6a2bd66abd6e6f29db3bac8c1a221d82c299cf
Munira1212 <muni0301@stud.kea.dk>
2023-04-03 14:34:02 +0200

    Merge remote-tracking branch 'origin/feat_1' into feat_3
    # Conflicts:
    #	src/main/java/com/example/makeawish/repository/UserRepo.java
    hi

--- src/main/java/com/example/makeawish/repository/UserRepo.java
+++ src/main/java/com/example/makeawish/repository/UserRepo.java
@@ -2,7 +2,12 @@ package com.example.makeawish.repository;
 
 public class UserRepo {
     public void hej(){
+
         System.out.println("EEEEJ");
+
+        System.out.println("hello");
+        System.out.println("HHHH");
+
     }
 
 }
