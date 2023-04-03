a6e79b57987474111a0ae87e020f07faf6e6346f
Munira1212 <muni0301@stud.kea.dk>
2023-04-03 14:02:16 +0200

    hi

---
+++ src/main/java/com/example/makeawish/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.makeawish.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+@Controller
+
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+
+        return "home/frontPage";
+    }
+
+
+}
